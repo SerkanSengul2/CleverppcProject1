52602826eed638447edc44a9c860084027ea5fc3
Serkan <serkansengul2@gmail.com>
2023-09-22 23:25:24 +0300

    deneme

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -40,7 +40,14 @@ public class DialogContent extends Parent {
     @FindBy(css = "p[class='alert alert-success']")
     public WebElement Verify;
 
+    @FindBy(css = "a[class='product-name']")
+    public List<WebElement> allSummerDresses;
 
+    @FindBy(css = "button[name='Submit'] > span")
+    public WebElement addToCartButton;
+
+    @FindBy(xpath = "(//span[@class='title'])[1]")
+    public WebElement confirmText;
 
 
     public WebElement ElementGet(String element) {
