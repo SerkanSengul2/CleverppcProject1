f2a55ae056889fb5402fc41dd1776c88a88927db
Serkan <ozsahinserkan@gmail.com>
2023-09-23 10:41:27 +0300

    All US's are done.

--- src/test/java/Pages/SOPages/SODialogContent.java
+++ src/test/java/Pages/SOPages/SODialogContent.java
@@ -34,4 +34,81 @@ public class SODialogContent extends Parent {
 
     @FindBy(css = "[id='product_list'] > li")
     public List<WebElement> allProducts;
+
+    @FindBy(css = "a[title='Sitemap']")
+    public WebElement sitemapButton;
+
+    @FindBy(css = "h1[class='page-heading']")
+    public WebElement sitemapText;
+
+    @FindBy(css = "a[title='View a list of my addresses']")
+    public WebElement addressButton;
+
+    @FindBy(css = "h1[class='page-heading']")
+    public WebElement addressText;
+
+    @FindBy(css = "a[title='Add an address']")
+    public WebElement newAddressButton;
+
+    @FindBy(id = "firstname")
+    public WebElement nameInput;
+
+    @FindBy(id = "lastname")
+    public WebElement lastnameInput;
+
+    @FindBy(id = "address1")
+    public WebElement addressInput;
+
+    @FindBy(id = "city")
+    public WebElement cityInput;
+
+    @FindBy(id = "id_state")
+    public WebElement state;
+
+    @FindBy(id = "postcode")
+    public WebElement zipCode;
+
+    @FindBy(id = "phone")
+    public WebElement phone;
+
+    @FindBy(id = "alias")
+    public WebElement addressAlias;
+
+    @FindBy(id = "submitAddress")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "(//h3[@class='page-subheading'])[2]")
+    public WebElement addressConfirm;
+
+    @FindBy(xpath = "(//a[@title='Delete'])[2]")
+    public WebElement deleteSecondAdress;
+
+    public WebElement getWebElement(String strElement) {
+
+        switch (strElement) {
+
+            case "nameInput": return this.nameInput;
+            case "lastnameInput": return this.lastnameInput;
+            case "addressInput": return this.addressInput;
+            case "cityInput": return this.cityInput;
+            case "zipCode": return this.zipCode;
+            case "phone": return this.phone;
+            case "addressAlias": return this.addressAlias;
+        }
+        return null;
+    }
+
+    // To Be DELETED
+
+    @FindBy(css = "div[class='header_user_info']")
+    public WebElement signInButton;
+
+    @FindBy(id = "email")
+    public WebElement emailText;
+
+    @FindBy(id = "passwd")
+    public WebElement passwordText;
+
+    @FindBy(id = "SubmitLogin")
+    public WebElement submitButton;
 }
