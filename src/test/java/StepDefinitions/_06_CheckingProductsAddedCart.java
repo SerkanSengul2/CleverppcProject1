420e699b85a47751d9c73c7186b2c6427523db1d
iremtorun <irmtorun@gmail.com>
2023-09-26 12:42:44 +0300

    Checking Products Added Cart Done

--- src/test/java/StepDefinitions/_06_CheckingProductsAddedCart.java
+++ src/test/java/StepDefinitions/_06_CheckingProductsAddedCart.java
@@ -1,15 +1,19 @@
 package StepDefinitions;
 
 import Pages.iremDialogContent;
+import Pages.iremLeftNav;
 import Utilities.GWD;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 
 
 public class _06_CheckingProductsAddedCart {
-     iremDialogContent dc=new iremDialogContent();
+    iremDialogContent dc = new iremDialogContent();
+    iremLeftNav ln = new iremLeftNav();
+
     @Given("navigate to dresses page")
     public void navigateToDressesPage() {
         GWD.getDriver().get("https://cleverppc.com/prestashop4/");
@@ -17,26 +21,24 @@ public class _06_CheckingProductsAddedCart {
         dc.MysendKeys(dc.email, "serkan@gmail.com");
         dc.MysendKeys(dc.password, "12345");
         dc.Myclick(dc.submitButton);
-
+        ln.Myclick(ln.dressesBtn);
     }
 
     @When("User chooses random products")
     public void userChoosesRandomProducts() {
+        for (int i = 0; i < 3; i++) {
+
+            int randomDress = (int) (Math.random() * dc.summerDress.size());
+            dc.summerDress.get(randomDress).click();
+            dc.Myclick(dc.addToCart);
+            dc.Myclick(dc.continueShopping);
+            GWD.getDriver().navigate().back();
+        }
+        dc.Myclick(dc.shoppingCart);
     }
 
-    @Then("User clicks on add to cart and user clicks on continue shopping button")
-    public void userClicksOnAddToCartAndUserClicksOnContinueShoppingButton() {
-    }
-
-    @And("User chooses a different product and user clicks on add to cart button")
-    public void userChoosesADifferentProductAndUserClicksOnAddToCartButton() {
-    }
-
-    @Then("User verifies that the added items are in the shopping cart")
-    public void userVerifiesThatTheAddedItemsAreInTheShoppingCart() {
-    }
-
-    @And("User verifies that there are three products in shopping cart")
+    @Then("User verifies that there are three products in shopping cart")
     public void userVerifiesThatThereAreThreeProductsInShoppingCart() {
+        Assert.assertTrue(dc.shoppingText.getText().contains("3"));
     }
 }
